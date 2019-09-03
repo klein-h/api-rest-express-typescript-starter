49694ee4ae6a879833bb315c7a16397f939e23cd
Loïc Pirez <loic@pirez.fr>
2019-09-03 21:16:52 +0200

    Changing code style to standard

--- lib/controllers/userController.ts
+++ lib/controllers/userController.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
 
 export class UserController {
-    public getUser(req: Request, res: Response) {
-        res.status(200).send({
-            message: 'GET REQUEST OK'
-        });
-        };
-    }
+  public getUser (req: Request, res: Response) {
+    res.status(200).send({
+      message: 'GET REQUEST OK'
+    })
+  }
+}
--- lib/routes/routes.ts
+++ lib/routes/routes.ts
@@ -1,9 +1,10 @@
-import {Request, Response} from "express";
-import { UserController } from "../controllers/userController";
+import { Application } from 'express'
+import { UserController } from '../controllers/userController'
+
 export class Routes {
-    public userController = new UserController();
-    public routes(app): void {
-        app.route('/')
-            .get(this.userController.getUser)
-    }
+  public userController = new UserController()
+  public routes (app: Application): void {
+    app.route('/')
+        .get(this.userController.getUser)
+  }
 }
