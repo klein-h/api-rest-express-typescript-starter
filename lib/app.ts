a1fa4fd38664941d24a5b8a8236de327f16a09c0
Arthur Klein <arthur.klein@epitech.eu>
2019-09-04 09:51:41 +0200

    Revert "Adjustments on the starter."

--- lib/app.ts
+++ lib/app.ts
@@ -1,21 +1,21 @@
-import * as express from 'express'
-import * as bodyParser from 'body-parser'
-import { Routes } from './routes/routes'
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Routes } from "./routes/routes";
 
 class App {
-  public app: express.Application
-  public routePrv: Routes = new Routes()
 
-  constructor () {
-    this.app = express()
-    this.config()
-    this.routePrv.routes(this.app)
-  }
+    public app: express.Application;
+    public routePrv: Routes = new Routes();
 
-  private config (): void {
-    this.app.use(bodyParser.json())
-    this.app.use(bodyParser.urlencoded({ extended: false }))
-  }
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routePrv.routes(this.app);
+    }
+
+    private config(): void{
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
 }
-
 export default new App().app
