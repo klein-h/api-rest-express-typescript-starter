0ba7e9d42cecf49f2eaa405cf001d3fcb8c2a9ba
Loïc PIREZ <loic@pirez.fr>
2019-09-04 09:39:20 +0200

    Adding return type on `getUser` function

--- lib/controllers/userController.ts
+++ lib/controllers/userController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 
 export class UserController {
-  public getUser (req: Request, res: Response) {
+  public getUser (req: Request, res: Response): void {
     res.status(200).send({
       message: 'GET REQUEST OK'
     })
